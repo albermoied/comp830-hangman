b2a69544c35cb5c07dc55c5a3a5326b1ab53c632
schug <sjh1033@wildcats.unh.edu>
2019-04-16 15:15:21 -0400

    Started main method for input - Scott

--- src/hangman/RunHangman.java
+++ src/hangman/RunHangman.java
@@ -1,10 +1,23 @@
 package hangman;
 
+import java.util.Scanner;
+import java.lang.Object;
+
 public class RunHangman {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Guess asterisk;
+		Guess guess;
+		String asterisks = asterisk.toString();
+		int count = guess.getCount();
 
+		while (count < 7 && asterisks.contains("*")) {
+			System.out.println("Guess any letter in the word");
+			System.out.println(asterisks);
+			String next = sc.next();
+			Guess(next);
+		}
+		sc.close();
 	}
-
 }
