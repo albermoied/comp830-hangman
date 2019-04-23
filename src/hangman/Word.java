b43ed189130014f92464857a34d9835901e776e9
schug <sjh1033@wildcats.unh.edu>
2019-04-23 15:00:08 -0400

    add EasyWord, MediumWord, HardWord objects and get methods

--- src/hangman/Word.java
+++ src/hangman/Word.java
@@ -1,9 +1,5 @@
 package hangman;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Word {
 
 	private String word;
@@ -19,41 +15,19 @@ public class Word {
 	}
 
 	public void retrieveWord() {
+		EasyWord easy = new EasyWord();
+		MediumWord medium = new MediumWord();
+		HardWord hard = new HardWord();
+		
 		if (difficulty == 1) {
-			// word = getEasyWord();
-			word = "plane";
+			word = easy.getEasyWord();
+			//word = "plane";
 		} else if (difficulty == 2) {
-			// word = getMediumWord();
-			word = "giraffe";
+			word = medium.getMediumWord();
+			//word = "giraffe";
 		} else {
-			// word = getHardWord();
-			word = "dandelion";
+			word = hard.getHardWord();
+			//word = "dandelion";
 		}
 	}
-
-	/*public String getEasyWord() {
-		return getRandomWordFromFile("easy.txt");
-	}
-
-	public String readFileToString(String path) {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(path));
-
-			try {
-				StringBuilder sb = new StringBuilder();
-				String line = br.readLine();
-				while (line != null) {
-					sb.append(line);
-					line = br.readLine();
-				}
-				return sb.toString();
-			} finally {
-				br.close();
-			}
-		} catch (IOException ioe) {
-			// Error handling of malformed path
-			System.out.println(ioe.getMessage());
-		}
-	}
-	*/
 }
