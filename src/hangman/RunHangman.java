3303627cf6854e5e83f35f045528a0aea7a390aa
Monica Pagliuca <46000098+MonicaPagliuca@users.noreply.github.com>
2019-04-26 22:38:28 -0400

    add validation to difficulty selection

--- src/hangman/RunHangman.java
+++ src/hangman/RunHangman.java
@@ -14,8 +14,17 @@ public class RunHangman {
 			System.out.println("Please choose a difficulty:");
 			System.out.println("Easy(1)--Medium(2)--Hard(3)");
 			int difficulty = sc.nextInt();
+			boolean Valid = false;
+
+			while (!Valid) {
+				System.out.println("Choose a difficulty between 1-3");
+				difficulty = sc.nextInt();
+				if (difficulty >= 1 && difficulty <= 3) {
+					Valid = true;
+				}
+			}
 			Guess guess = new Guess(difficulty);
-			
+
 			while (guess.getCount() < 7 && guess.getAsterisk().contains("*")) {
 				System.out.println("Guess any letter in the word");
 				System.out.println(guess.getAsterisk());
@@ -24,9 +33,9 @@ public class RunHangman {
 			}
 			System.out.println("The word was " + guess.getWord());
 			// done = true;
-			
+
 			boolean validate = false;
-			while(!validate) {
+			while (!validate) {
 				System.out.println("\nDo you want to play again?");
 				String answer = sc.next();
 
@@ -35,14 +44,13 @@ public class RunHangman {
 					sc.close();
 					reset = false; // break outer loop
 					validate = true; // break inner loop
-				}
-				else if (answer.equalsIgnoreCase("yes")) {
+				} else if (answer.equalsIgnoreCase("yes")) {
 					validate = true; // break inner loop
 				} else {
 					System.out.println("You inputted a wrong value. Please input yes or no.");
 				}
 			}
-				
+
 		}
 		// restart main method
 		// if restart ('y') selected, restart game. if restart cancelled ('n'),
