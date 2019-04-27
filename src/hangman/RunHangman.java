8edad140eebda3a666669065d54b4769002b044c
schug <sjh1033@wildcats.unh.edu>
2019-04-26 22:08:50 -0400

    add restart game feature

--- src/hangman/RunHangman.java
+++ src/hangman/RunHangman.java
@@ -5,30 +5,52 @@ import java.util.Scanner;
 public class RunHangman {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Choose a difficulty from 1-3: ");
-		int difficulty = sc.nextInt();
-		Guess guess = new Guess(difficulty);
-		
-		//if (sc.nextInt() > 3) {
-			//System.out.println("Please choose a difficulty 1-3");
-		//}
+		// boolean done = true;
+		boolean reset = true;
 
-		while (guess.getCount() < 7 && guess.getAsterisk().contains("*")) {
-			System.out.println("Guess any letter in the word");
-			System.out.println(guess.getAsterisk());
-			String next = sc.next();
-			guess.makeGuess(next);
+		while (reset) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Welcome to HANGMAN!" + "\n");
+			System.out.println("Please choose a difficulty:");
+			System.out.println("Easy(1)--Medium(2)--Hard(3)");
+			int difficulty = sc.nextInt();
+			Guess guess = new Guess(difficulty);
+			
+			while (guess.getCount() < 7 && guess.getAsterisk().contains("*")) {
+				System.out.println("Guess any letter in the word");
+				System.out.println(guess.getAsterisk());
+				String next = sc.next();
+				guess.makeGuess(next);
+			}
+			System.out.println("The word was " + guess.getWord());
+			// done = true;
+			
+			boolean validate = false;
+			while(!validate) {
+				System.out.println("\nDo you want to play again?");
+				String answer = sc.next();
+
+				if (answer.equalsIgnoreCase("no")) {
+					System.out.println("GOOD-BYE, THANKS FOR PLAYING");
+					sc.close();
+					reset = false; // break outer loop
+					validate = true; // break inner loop
+				}
+				else if (answer.equalsIgnoreCase("yes")) {
+					validate = true; // break inner loop
+				} else {
+					System.out.println("You inputted a wrong value. Please input yes or no.");
+				}
+			}
+				
 		}
-		System.out.println("The word was " + guess.getWord());
-		System.out.println("\n" + "To restart the game press ");
-		//restart main method 
-		//if restart ('y') selected, restart game. if restart cancelled ('n'), sc.close();
-		//look up scanner methods ie. sc.nextInt()
-		sc.close();
-		
-		//File file = new File(".");
-		//for(String fileNames : file.list()) System.out.println(fileNames);
-		//use above to make sure text files were in the right path
+		// restart main method
+		// if restart ('y') selected, restart game. if restart cancelled ('n'),
+		// sc.close();
+		// look up scanner methods ie. sc.nextInt()
+
+		// File file = new File(".");
+		// for(String fileNames : file.list()) System.out.println(fileNames);
+		// use above to make sure text files were in the right path
 	}
 }
