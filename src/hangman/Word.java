9fd50808f9e19cf99002df49f1aec8e2e2854d8b
Alber Moied <am1545@wildcats.unh.edu>
2019-04-29 19:41:50 -0400

    added a switch statement to retrieve word according to difficulty

--- src/hangman/Word.java
+++ src/hangman/Word.java
@@ -7,18 +7,19 @@ public class Word {
 
 	public Word(int difficulty) {
 		this.difficulty = difficulty;
-		retrieveWord();
+		retrieveWord(difficulty);
 	}
 
 	public String getWord() {
 		return word;
 	}
 
-	public void retrieveWord() {
-		EasyWord easy = new EasyWord();
-		MediumWord medium = new MediumWord();
-		HardWord hard = new HardWord();
+	public void retrieveWord(int diff) {
+		//EasyWord easy = new EasyWord();
+		//MediumWord medium = new MediumWord();
+		//HardWord hard = new HardWord();
 		
+		/*
 		if (difficulty == 1) {
 			word = easy.getEasyWord();
 			//word = "plane";
@@ -28,6 +29,33 @@ public class Word {
 		} else {
 			word = hard.getHardWord();
 			//word = "dandelion";
+		}*/
+		
+		
+		switch(difficulty) {
+		
+		case 1:
+			EasyWord easy = new EasyWord();
+			word = easy.getEasyWord();
+			break;
+			
+		case 2:
+			MediumWord medium = new MediumWord();
+			word = medium.getMediumWord();
+			break;
+			
+		case 3:
+			HardWord hard = new HardWord();
+			word = hard.getHardWord();
+			break;
+			
+		/*default:
+			RunHangman run = new RunHangman();
+			System.out.println("Wrong Selection. Try again...");
+			word = "";
+			//run.main(null);
+		break;*/
+		
 		}
 	}
 }
